8948a000a810676e515cad05d832f9361350404c
rogan-macleod <roganemacleod@gmail.com>
2024-10-25 10:51:03 +0100

    worksheet4

--- prac4.py
+++ prac4.py
@@ -1,4 +1,29 @@
-def test():
-    for i in range(10):
-        print("hello git")
-test()
+import os
+def personal_greeting():
+    name = input("please enter your name ")
+    print(f"Hello {name}, nice to see you!")
+
+def formal_name():
+    namef = input("enter your forename ")
+    nameS =input("enter your surname ")
+    namef_formal = namef[0]
+    print(f"{namef_formal}. {nameS}")
+
+def kilos_to_ounces():
+    kilos = float(input("enter kilos "))
+    ounces = kilos * 35.2739619
+    kilos = round(kilos,2)
+    ounces = round(ounces,2)
+    print(f"here is {kilos} and {ounces}")
+
+def generate_surnameemail():
+    forename = input("enter your first name ")
+    forename = forename[0]
+    surname = input("enter you last name ")
+    year = input("enter the year you joined uni")
+    surname = surname[:4]
+    email_back = "@myport.ac.uk"
+    email = surname + "." + forename +"."+year+email_back
+    print(email)
+
+generate_surnameemail()
