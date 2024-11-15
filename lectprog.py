452bbf288b07ded6ea35a376e66b634365050161
rogan-macleod <roganemacleod@gmail.com>
2024-11-15 09:12:50 +0000

    test

---
+++ lectprog.py
@@ -0,0 +1,11 @@
+def therm(temp):
+    if temp < 20:
+        print("heat")
+    elif  (temp < 23):
+        print("nothing")
+    elif (temp < 25):
+        print("fans no cooling")
+    else:
+        print("turns on cooling")
+
+
