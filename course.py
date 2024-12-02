84c5f79d8bbdf2bf60ff2787b17fdc6f69649849
rogan-macleod <roganemacleod@gmail.com>
2024-12-02 17:07:36 +0000

    coursework

--- course.py
+++ course.py
@@ -38,16 +38,45 @@ def patches(win,colour2,size,colour3):
                 rectangle(win,tl,br,colour3,colour3)
 
 
-def pen_patch(win,colour2,y,x):
-    tl = Point(x,y)
-    start_x = tl.x
-    start_y = tl.y
-    for Y in range(start_y , start_y+100,20):
-        for X in range(start_x,start_x+100,20):
-            p1 = Point(X,Y)
-            p2 = Point(X+20,Y+20)
-            rectangle(win,p1,p2,colour2,colour2)
-                  
+
+def patch2(win,colour3,size,x,y):
+    win= Window("",500,500)
+    colour3 = "red"
+    size = 500
+    test = Point(x,y)
+    start_x = test.x
+    start_y = test.y
+    for Y in range(100,start_y+size-100,10):
+        for X in range(100,start_x+size-100,10):
+            tl = Point(Y,X)
+            mod = Y % 100
+            if mod % 2 == 0:
+                br = Point(Y+5,X+5)
+            else:
+                br = Point(Y+10,X+10)
+        rectangle(win,tl,br,"black",colour3)
+
+
+
+
+def pen_patch(win,colour,y,x):
+   p = Point(x,y)
+   start_x = p.x
+   start_y = p.y
+   for X in range(start_x,start_x+100,10):
+       if (X//10)%2 == 1:
+           for Y in range(start_y,start_y+100,25):
+               tl = Point(X,Y)
+               br = Point(X+10,Y+25)
+               rectangle(win,tl,br,"black",colour)
+       else:
+            for Y2 in range(start_y,start_y+100,20):
+                tl = Point(X,Y2)
+                br = Point(X+10,Y2+20)
+                if (Y2//20)%2 == 0:
+                    rectangle(win,tl,br,"black",colour)
+                else:
+                    rectangle(win,tl,br,"black","white")
 def draw_patch_window(win,colour,y,x):
     tl = Point(x,y)
     start_x = tl.x
@@ -60,11 +89,10 @@ def draw_patch_window(win,colour,y,x):
             rectangle(win,p1,p2,colour,"white")
             text(win,t1,"hi!",5,colour)
 
-
-
-def main():
+def check_vals():
     val_sizes = [5,7,9]
     val_colours = ["red","green","blue","magenta","orange","purple"]
+
     size = int(input("enter a window sixe (5,7,9): "))
     while size not in val_sizes:
         size = int(input("not valid size enter a valid size: "))
@@ -82,27 +110,32 @@ def main():
     colour3 = input("enter a colour: ")
     while colour3.lower() not in val_colours:
         colour3=input("not valid colour enter a valid colour: ")
+    return size,colour,colour2,colour3
+
+
+def check_keys(win):
+    while True:
+        key = win.get_key()
+        print(key)
+def main():
+    size,colour,colour2,colour3 = check_vals()
 
     win = Window("up2274850",size,size)
+    patches(win,colour2,size,colour3)
     for y in range(0,size,200):
         rectangle2(win,y+100,colour,size)
         for x in range(0,size,100):
             if y == 0 or y == size-100:
                 draw_patch_window(win,colour,y,x)
+            elif (y//100)%2 == 0:
+                pen_patch(win,colour3,y,x)
+            if (y//100)%2 == 0 and (x+y) > size-100:
+                draw_patch_window(win,colour2,y,x)
             draw_patch_window(win,colour,y,0)
             draw_patch_window(win,colour,y,size-100)
-        #            if (size-100) < (x+ y):
- #               pen_patch(win,colour2,y,x)
-  #          if (size-100) > (x+ y) or (size-100) == (x+y) :
-   #             pen_patch(win,colour3,y,x)
-#        elif (y % 200) == 0:
- #           for x2 in range(100,size-100,100):
-  #              pen_pacth(win,colour2,y,x2)
-            
-        
-    
 
-    patches(win,colour2,size,colour3)
+         
+    check_keys(win)
     win.get_mouse()
     win.close()
 
