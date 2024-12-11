1325b63f8f00a65c55715555456eb90184a9bad3
rogan-macleod <roganemacleod@gmail.com>
2024-12-11 11:49:07 +0000

    real2

--- coursereal.py
+++ coursereal.py
@@ -5,6 +5,7 @@ def rectangle(win,tl,br,out_colour,colour):
     rect.outline_colour = out_colour
     rect.fill_colour = colour
     rect.draw(win)
+    return rect
 
 
 
@@ -17,6 +18,7 @@ def text(win,point,colour,size):
 
 
 def pen_patch(win,colour,x,y):
+    patch_obj = []
     start_x = x
     start_y = y
     for row in range(start_x,start_x+100,10):
@@ -24,15 +26,19 @@ def pen_patch(win,colour,x,y):
             for column in range(start_y,start_y+100,25):
                 tl = Point(row,column)
                 br = Point(row+10,column+25)
-                rectangle(win,tl,br,"black",colour)
+                rect_long = rectangle(win,tl,br,"black",colour)
+                patch_obj.append((tl,br))
         else:
             for column2 in range(start_y,start_y+100,20):
                 tl = Point(row,column2)
                 br = Point(row+10,column2+20)
                 if (column2//20)%2 ==0:
-                    rectangle(win,tl,br,"black",colour)
+                    rect_short = rectangle(win,tl,br,"black",colour)
+                    patch_obj.append((tl,br))
                 else:
-                    rectangle(win,tl,br,"black","white")
+                    rect_short_other = rectangle(win,tl,br,"black","white")
+                    patch_obj.append((tl,br))
+    print(patch_obj)
 
 def draw_final_patch(win,colour,x,y):
     start_x = x
@@ -86,14 +92,14 @@ def loops(win,size,colour,colour2,colour3):
                 draw_final_patch(win,colour,row,column)
             elif (column//100)%2 == 0:
                 pen_patch(win,colour3,row,column)
-            if (column//100)%2 == 0 and (column+row) > size-100 and not column== size-100:
+            if (column//100)%2 == 0 and (column+row) > size-100 and not column == size-100:
                 draw_final_patch(win,colour2,row,column)
             elif (column//100)%2 == 1 and (column+row) > size-100:
                     rectangle(win,Point(row,column),Point(row+100,column+100),colour2,colour2)
             elif (column//100)%2 == 1 and (column+row) <= size-100:
                     rectangle(win,Point(row,column),Point(row+100,column+100),colour3,colour3)
-            draw_final_patch(win,colour,0,column)
-            draw_final_patch(win,colour,size-100,column)
+            #draw_final_patch(win,colour,0,column)
+            #draw_final_patch(win,colour,size-100,column)
 
 
 def main():
