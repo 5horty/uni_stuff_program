e8a0016cf77a87ef23aa6315d58e2cbd55da7f5a
rogan-macleod <roganemacleod@gmail.com>
2024-12-15 20:45:44 +0000

    gooood

--- lasts.py
+++ lasts.py
@@ -11,7 +11,7 @@ def rectangle(win,tl,br,out_colour,colour):
 def border(win,p1):
     br = Point(p1.x + 100,p1.y + 100)
     rect = Rectangle(p1,br)
-    rect.outline_width = 5
+    rect.outline_width = 8
     rect.outline_colour = "black"
     rect.draw(win)
     return rect
@@ -24,16 +24,17 @@ def text(win,point,colour,size):
     return texts
 
 def undraw_stuff(patch_obj):
-    if isinstance(patch_obj,list):
-        for i in patch_obj:
-            i.undraw()
+    if type(patch_obj) == list:
+        for item in patch_obj:
+            item.undraw()
     else:
         patch_obj.undraw()
+
 def move_patch(patch_obj,x_amount,y_amount):
-    if isinstance(patch_obj,list):
+    if type(patch_obj) == list:
         for item in patch_obj:
             item.move(x_amount,y_amount)
-            time.sleep(0.1)
+            time.sleep(0.01)
     else:
         patch_obj.move(x_amount,y_amount)
     return patch_obj 
@@ -59,7 +60,7 @@ def pen_patch(win,colour,x,y):
             for column2 in range(start_y,start_y+100,20):
                 tl = Point(row,column2)
                 br = Point(row+10,column2+20)
-                if (column2//20)%2 ==0:
+                if ((column2-start_y)//20)%2 ==0:
                     rect_short = rectangle(win,tl,br,"black",colour)
                     patch_obj.append(rect_short)
                 else:
@@ -69,7 +70,7 @@ def pen_patch(win,colour,x,y):
     
     #undraw_stuff(patch_obj)
     
-def check_keys(win,grid,colour,colour2,colour3):
+def check_keys(win,grid,colour,colour2,colour3,size):
     border_grid = {}
     click = win.get_mouse()
     patch_border_x = click.x//100*100
@@ -83,13 +84,7 @@ def check_keys(win,grid,colour,colour2,colour3):
         match key:
             case "x":
                 undraw_stuff(grid[(patch_x,patch_y)])
-                if (patch_x,patch_y) in grid:
-                    print("in")
-                    print(len(grid))
                 grid.pop((patch_x,patch_y))
-                print(len(grid))
-                if (patch_x,patch_y) not in grid:
-                    print("out")
             case "1":
                 if (patch_x,patch_y)not in grid:
                     patch = pen_patch(win,colour,patch_x,patch_y)
@@ -116,25 +111,42 @@ def check_keys(win,grid,colour,colour2,colour3):
                     grid[(patch_x,patch_y)] = patch
             case "7":
                 if (patch_x,patch_y)not in grid:
-                    patch = draw_final_patch(win,Point(patch_x,patch_y),Point(patch_x+100,patch_y+100),colour,colour)
+                    tl = Point(patch_x,patch_y)
+                    br = Point(patch_x+100,patch_y+100)
+                    patch = draw_final_patch(win,tl,br,colour,colour)
                     grid[(patch_x,patch_y)] = patch
             case "8":
                 if (patch_x,patch_y)not in grid:
-                    patch = rectangle(win,Point(patch_x,patch_y),Point(patch_x+100,patch_y+100),colour2,colour2)
+                    tl = Point(patch_x,patch_y)
+                    br = Point(patch_x+100,patch_y+100)
+                    patch = rectangle(win,tl,br,colour2,colour2)
                     grid[(patch_x,patch_y)] = patch
             case "9":
                 if (patch_x,patch_y)not in grid:
-                    patch = rectangle(win,Point(patch_x,patch_y),Point(patch_x+100,patch_y+100),colour3,colour3)
+                    tl = Point(patch_x,patch_y)
+                    br = Point(patch_x+100,patch_y+100)
+                    patch = rectangle(win,tl,br,colour3,colour3)
                     grid[(patch_x,patch_y)] = patch
             case "Up":
                 if (patch_x,patch_y-100)not in grid and patch_y >= 0:
                     patch = move_patch(grid[(patch_x,patch_y)],0,-100)
-                    if (patch_x,patch_y) in grid:
-                        print("still in")
                     grid.pop((patch_x,patch_y))
-                    if (patch_x,patch_y)not in grid:
-                        print("not in")
                     grid[(patch_x,patch_y-100)] = patch
+            case "Down":
+                if (patch_x,patch_y+100)not in grid and patch_y+100 <= size:
+                    patch = move_patch(grid[(patch_x,patch_y)],0,100)
+                    grid.pop((patch_x,patch_y))
+                    grid[(patch_x,patch_y+100)] = patch
+            case "Left":
+                if (patch_x-100,patch_y)not in grid and patch_x >= 0:
+                    patch = move_patch(grid[(patch_x,patch_y)],-100,0)
+                    grid.pop((patch_x,patch_y))
+                    grid[(patch_x-100,patch_y)] = patch
+            case "Right":
+                if (patch_x+100,patch_y)not in grid and patch_x <= size :
+                    patch = move_patch(grid[(patch_x,patch_y)],+100,0)
+                    grid.pop((patch_x,patch_y))
+                    grid[(patch_x+100,patch_y)] = patch
             case "Escape":
                 undraw_stuff(border_grid[(patch_border_x,patch_border_y)])
                 border_grid.pop((patch_border_x,patch_border_y))
@@ -164,26 +176,37 @@ def draw_final_patch(win,colour,x,y):
     return patch_obj
 
 def check_vals():
-    val_sizes = [5,7,9]
+    val_sizes = ["5","7","9"]
     val_colours = ["red","green","blue","magenta","orange","purple"]
 
-    size = int(input("enter a window sixe (5,7,9): "))
+    size = input("Enter a window size (5,7,9): ")
+    size = size.strip()
     while size not in val_sizes:
-        size = int(input("not valid size enter a valid size: "))
+        size = input("Not valid size enter a valid size: ")
+        size = size.strip()
+    size = int(size)
     size = size * 100
 
-    colour = input("enter a colour: ")
-    while colour.lower() not in val_colours:
-        colour = input("not valid colour enter a valid colour: ")
+    colour = input("""Enter the first colour 
+(red, greem, blue, magenta, orange, purple): """)
+    colour = colour.strip()
+    while colour.lower() not in val_colours or colour.isdigit():
+        colour = input("Not valid colour enter a valid colour: ")
+        colour = colour.strip()
         
-    colour2 = input("enter a colour: ")
-    while colour2.lower() not in val_colours:
-        colour2= input("not valid colour enter a valid colour: ")
+    colour2 = input("Enter the second colour: ")
+    colour2 = colour2.strip()
+    while colour2.lower() not in val_colours or colour2.isdigit():
+        colour2 = input("Not valid colour enter a valid colour: ")
+        colour2 = colour2.strip()
+        
 
 
-    colour3 = input("enter a colour: ")
-    while colour3.lower() not in val_colours:
-        colour3=input("not valid colour enter a valid colour: ")
+    colour3 = input("Enter the third colour: ")
+    colour3 = colour3.strip()
+    while colour3.lower() not in val_colours or colour.isdigit():
+        colour3 = input("Not valid colour enter a valid colour: ")
+        colour3 = colour3.strip()
 
     return size,colour,colour2,colour3
     
@@ -213,20 +236,28 @@ def loops(win,size,colour,colour2,colour3):
                 grid[(row,column)] = patch
             else:
                 if row >= 100 and row <= size-100:
+                    tl = Point(row,column)
+                    br = Point(row+100,column+100)
                     if (column//100)%2 == 0 and (column+row) <= size-100:
                         patch = pen_patch(win,colour3,row,column)
                         grid[(row,column)] = patch
-                    if (column//100)%2 == 0 and (column+row) > size-100 and row+100 <= size-100:
+                    if (column//100)%2 == 0  \
+                            and (column+row) > size-100 \
+                            and row+100 <= size-100:
+
                         patch = draw_final_patch(win,colour2,row,column)
                         grid[(row,column)] = patch
-                    if (column//100)%2 == 1 and (column+row) > size-100 and row+100 <= size-100:
-                        patch = rectangle(win,Point(row,column),Point(row+100,column+100),colour2,colour2)
+                    if (column//100)%2 == 1 \
+                            and (column+row) > size-100 \
+                            and row+100 <= size-100:
+
+                        patch = rectangle(win,tl,br,colour2,colour2)
                         grid[(row,column)] = patch
                     if (column//100)%2 == 1 and (column+row) <= size-100:
-                        patch = rectangle(win,Point(row,column),Point(row+100,column+100),colour3,colour3)
+                        patch = rectangle(win,tl,br,colour3,colour3)
                         grid[(row,column)] = patch
     
-    check_keys(win,grid,colour,colour2,colour3)
+    check_keys(win,grid,colour,colour2,colour3,size)
 
 
 def main():
