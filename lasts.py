30fb7fe6df677f6181ae3cadd72ff6911ca7c019
rogan-macleod <roganemacleod@gmail.com>
2024-12-16 01:41:27 +0000

    work

--- lasts.py
+++ lasts.py
@@ -1,13 +1,13 @@
 from graphix import Rectangle,Text,Window,Point
 import time
-
+#rectangle helper function 
 def rectangle(win,tl,br,out_colour,colour):
     rect = Rectangle(tl,br)
     rect.outline_colour = out_colour
     rect.fill_colour = colour
     rect.draw(win)
     return rect
-
+#border frame that goes on top
 def border(win,p1):
     br = Point(p1.x + 100,p1.y + 100)
     rect = Rectangle(p1,br)
@@ -15,21 +15,21 @@ def border(win,p1):
     rect.outline_colour = "black"
     rect.draw(win)
     return rect
-
+#text inside the final patch boxs
 def text(win,point,colour,size):
     texts = Text(point,"hi!")
     texts.fill_colour = colour
     texts.size = size
     texts.draw(win)
     return texts
-
+#function that undraws objs for challenge
 def undraw_patches(patch_obj):
     if type(patch_obj) == list:
         for item in patch_obj:
             item.undraw()
     else:
         patch_obj.undraw()
-
+#function that moves patchs by specified amount and animation
 def move_patch(patch_obj,x_amount,y_amount):
     if type(patch_obj) == list:
         for item in patch_obj:
@@ -41,42 +41,42 @@ def move_patch(patch_obj,x_amount,y_amount):
         patch_obj.move(x_amount//2,y_amount//2)
 
     return patch_obj 
-    
+#draws the border around where the patch the user clicks on  
 def box_click(win,tl_point):
     xclick = (tl_point.x//100)*100
     yclick = (tl_point.y//100)*100
     patch_border = border(win,Point(xclick,yclick))
     return patch_border
-   
+#pen_patch    
 def pen_patch(win,colour,x,y):
     patch_obj = []
     start_x = x
     start_y = y
     for row in range(start_x,start_x+100,10):
-        if (row//10)%2 == 1:
+        if (row//10)%2 == 1:                #if row is even every second one
             for column in range(start_y,start_y+100,25):
                 tl = Point(row,column)
                 br = Point(row+10,column+25)
                 rect_long = rectangle(win,tl,br,"black",colour)
                 patch_obj.append(rect_long)
-        else:
-            for column2 in range(start_y,start_y+100,20):
-                tl = Point(row,column2)
-                br = Point(row+10,column2+20)
-                if ((column2-start_y)//20)%2 ==0:
+        else:                               #if row is odd
+            for column_odd in range(start_y,start_y+100,20):
+                tl = Point(row,column_odd) #makes it draw right way every row
+                br = Point(row+10,column_odd+20)
+                if ((column_odd-start_y)//20)%2 ==0: 
                     rect_short = rectangle(win,tl,br,"black",colour)
                     patch_obj.append(rect_short)
                 else:
                     rect_short_other = rectangle(win,tl,br,"black","white")
                     patch_obj.append(rect_short_other)
     return patch_obj
-    
-    #undraw_stuff(patch_obj)
+#helper function that draws a patch and updates the grid
 def add_patch(win, grid, patch_x, patch_y, colour, draw_func):
     if (patch_x, patch_y) not in grid:
         patch = draw_func(win, colour, patch_x, patch_y)
         grid[(patch_x, patch_y)] = patch
 
+#helper function that draws a rectangle and updates the grid
 def add_block(win, grid,patch_x, patch_y, colour, draw_func):
     if (patch_x, patch_y) not in grid:
         tl = Point(patch_x,patch_y)
@@ -103,7 +103,7 @@ def check_keys(win,grid,colour,colour2,colour3,size):
                 else:
                     undraw_patches(grid[(patch_x,patch_y)])
                     grid.pop((patch_x,patch_y))
-            case "1":
+            case "1": #if a key is pressed draws corrisponding patch
                 add_patch(win, grid, patch_x, patch_y, colour, pen_patch)
             case "2":
                 add_patch(win, grid, patch_x, patch_y, colour2, pen_patch)
@@ -121,7 +121,7 @@ def check_keys(win,grid,colour,colour2,colour3,size):
                 add_block(win,grid,patch_x,patch_y,colour2,rectangle)
             case "9":
                 add_block(win,grid,patch_x,patch_y,colour3,rectangle)
-            case "Up":
+            case "Up":#updates patch coordintes and moves patch accordingly
                 if (patch_x,patch_y-100)not in grid \
                         and patch_y-100 >= 0 \
                         and (patch_x,patch_y) in grid:
@@ -130,7 +130,7 @@ def check_keys(win,grid,colour,colour2,colour3,size):
                     grid[(patch_x,patch_y-100)] = patch
             case "Down":
                 if (patch_x,patch_y+100)not in grid \
-                        and patch_y+100 <= size \
+                        and patch_y+100 < size \
                         and (patch_x,patch_y) in grid:
                     patch = move_patch(grid[(patch_x,patch_y)],0,100)
                     grid.pop((patch_x,patch_y))
@@ -144,9 +144,9 @@ def check_keys(win,grid,colour,colour2,colour3,size):
                     grid[(patch_x-100,patch_y)] = patch
             case "Right":
                 if (patch_x+100,patch_y)not in grid \
-                        and patch_x+100 <= size \
+                        and patch_x+100 < size \
                         and (patch_x,patch_y) in grid:
-                    patch = move_patch(grid[(patch_x,patch_y)],+100,0)
+                    patch = move_patch(grid[(patch_x,patch_y)],100,0)
                     grid.pop((patch_x,patch_y))
                     grid[(patch_x+100,patch_y)] = patch
             case "Escape":
@@ -161,7 +161,7 @@ def check_keys(win,grid,colour,colour2,colour3,size):
 
 
 
-
+#draws the final patch
 def final_patch(win,colour,x,y):
     patch_obj = []
     start_x = x
@@ -176,7 +176,7 @@ def final_patch(win,colour,x,y):
             texts = text(win,point,colour,5)
             patch_obj.append(texts)
     return patch_obj
-
+#checks for the user input and does validation checks
 def check_vals():
     val_sizes = ["5", "7", "9"]
     val_colours = ["red", "green", "blue", "magenta", "orange", "purple"]
@@ -186,16 +186,17 @@ def check_vals():
         size = input("Not valid size! Enter a valid size (5, 7, 9): ").strip()
     size = int(size) * 100
 
-    selected_colours = set()  # To track chosen colours
+    selected_colours = set()  # to track chosen colours
 
-    def get_unique_colour(prompt):
+    def get_unique_colour(prompt):#gets each colour 
         while True:
             colour = input(prompt).strip().lower()
             if colour in val_colours and colour not in selected_colours:
                 selected_colours.add(colour)
                 return colour
             elif colour in selected_colours:
-                print(f"You've already chosen '{colour}'. Please pick a different colour.")
+                print(f"""You've already chosen '{colour}'.
+Please pick a different colour. """)
             else:
                 print("Not a valid colour! Enter a valid colour.")
 
@@ -208,13 +209,14 @@ def check_vals():
 
 def loops(win,size,colour,colour2,colour3):
     grid = {}
-    
-    
+     
     for column in range(0,size,100):
         if (column//100)%2 == 0 and not column ==0 and not column == size-100:
+            #draws final patch in left and right most columns in mid patchwork
             add_patch(win, grid, size-100, column, colour, final_patch)
             add_patch(win, grid, 0, column, colour, final_patch)
         if (column//100)%2 == 1:
+            #draws rectangle in left and right most columns in mid patchwork
             tl = Point(0,column)
             br = Point(100,column+100)
             patch = rectangle(win,tl,br,colour,colour)
@@ -224,6 +226,7 @@ def loops(win,size,colour,colour2,colour3):
             patch = rectangle(win,tl,br,colour,colour)
             grid[(tl.x,tl.y)] = patch
         for row in range(0,size,100):
+            #draws the main inside square with 'stairs'
             if column == 0 or column == size-100:
                 add_patch(win, grid, row, column, colour, final_patch)
             else:
@@ -250,7 +253,7 @@ def loops(win,size,colour,colour2,colour3):
     check_keys(win,grid,colour,colour2,colour3,size)
     
     win.close()
-
+#main function
 def main():
     size,colour,colour2,colour3 = check_vals()
     win = Window("",size,size)
