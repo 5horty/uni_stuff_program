20903a9d0529fff4444c75ae3e9a89ba2660f56e
rogan-macleod <roganemacleod@gmail.com>
2025-03-15 18:57:12 +0000

    better

--- cw2/front22.py
+++ cw2/front22.py
@@ -3,6 +3,7 @@ from tkinter import Tk, Frame,Label,Button,StringVar
 
 class SmartHomeApp:
     buttons = {}
+    labels ={}
 
     def __init__(self):
         self.win = Tk()
@@ -22,26 +23,40 @@ class SmartHomeApp:
 
 
     def run(self):
-        self.create_labels()
+        #self.create_labels()
         self.create_buttons()
         self.win.mainloop()
 
+    def update_labels(self):
+        for device_id, item in self.labels.items():
+            index = next((i for i, d in enumerate(self.smart_home.device_list) if id(d) == device_id),None)
+            if index is not None:
+                device = self.smart_home.device_list[index]
+                text = f"{index+1}: {str(device)}"
+                item.config(text = text)
+
     def turn_on_all(self):
         self.smart_home.switch_all_on()
-        self.strvar.set(str(self.smart_home))
+        self.update_labels()
+
+    def turn_off_all(self):
+        self.smart_home.switch_all_off()
+        self.update_labels()
+
 
     def toggle_btn(self,device_id):
         index = next((i for i, d in enumerate(self.smart_home.device_list) if id(d) == device_id),None)
         if index is not None:
             self.smart_home.device_list[index].toggle_switch()
-            self.strvar.set(str(self.smart_home))
+            self.update_labels()
 
     def delete_btn(self,device_id):
         index = next((i for i, d in enumerate(self.smart_home.device_list) if id(d) == device_id),None)
         if index is not None:
             self.smart_home.remove_device(index)
-            self.strvar.set(str(self.smart_home))
+            self.remove_labels(device_id)
             self.remove_btns(device_id)
+            self.update_labels()
 
     def remove_btns(self,device_id):
         if device_id in self.buttons:
@@ -49,13 +64,19 @@ class SmartHomeApp:
             for btn in self.buttons[device_id]:
                 btn.destroy()
         self.buttons.pop(device_id)
+    
+    def remove_labels(self,device_id):
+        if device_id in self.labels:
+            self.labels[device_id].destroy()
+            self.labels.pop(device_id)
 
     def create_labels(self):
-        smarthome = Label(
-            self.main_frame,
-            textvariable= self.strvar 
-        )
-        smarthome.grid(row = 1,column= 0,columnspan=2,sticky= "nesw")
+        for i , items in enumerate(self.smart_home.device_list):
+            devices = Label(
+                self.main_frame,
+                text= f"{i+1}: {str(items)}"
+            )
+            devices.grid(row = 1,column= 0,columnspan=2,rowspan=row,sticky= "nesw")
 
     def create_buttons(self):
         turn_on_btn = Button(
@@ -67,7 +88,7 @@ class SmartHomeApp:
         turn_off_btn = Button(
             self.main_frame,
             text = "turn off all",
-            command= self.turn_on_all
+            command= self.turn_off_all
         )
         turn_off_btn.grid(row = 0,column=2,columnspan=2,sticky="nesw")
 
@@ -80,17 +101,30 @@ class SmartHomeApp:
                 text = "toggle",
                 command= lambda id = device_id: self.toggle_btn(id)
                 )
-            toggle_btn.grid(row = 2+i,column= 2,sticky="nesw")
+            toggle_btn.grid(row = 1+i,column= 2,sticky="nesw")
             delete_btn = Button(
                 self.main_frame,
                 text = "delete",
                 command= lambda id = device_id :self.delete_btn(id)
                 )
-            delete_btn.grid()
+            delete_btn.grid(row=1+i,column=4)
+            edit_btn = Button(
+                self.main_frame,
+                text = "edit",
+                command= lambda id = device_id :self.delete_btn(id)
+                )
+            edit_btn.grid(row=1+i,column=3)
+
+            devices = Label(
+                self.main_frame,
+                text= f"{i+1}: {str(items)}"
+            )
+            devices.grid(row = i+1,column= 0,columnspan=2,sticky= "nesw")
             buttons_row.append(toggle_btn)
             buttons_row.append(delete_btn)
             print(buttons_row)
-            self.buttons[device_id] = [toggle_btn,delete_btn]
+            self.buttons[device_id] = [toggle_btn,delete_btn,edit_btn]
+            self.labels[device_id] = devices
             print(self.buttons)
             
 
